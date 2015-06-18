c9138286dc7303ac05062cb56db27b09052bc526
AlecTheDev <a.rasch@sportradar.com>
2015-06-18 15:38:20 +0200

    Updated unittests for is_prime function.

--- tests/math_helpers_tests.py
+++ tests/math_helpers_tests.py
@@ -8,11 +8,15 @@ import helpers.math_helper as mh
 class TestPrimeMethods(unittest.TestCase):
 
     def test_is_prime(self):
+        self.assertFalse(mh.is_prime(1))
+        self.assertTrue(mh.is_prime(2))
+        self.assertTrue(mh.is_prime(3))
+        self.assertFalse(mh.is_prime(4))
+        self.assertFalse(mh.is_prime(9))
+        self.assertTrue(mh.is_prime(11))
+        self.assertTrue(mh.is_prime(1709))
+        self.assertTrue(mh.is_prime(5381))
+        self.assertTrue(mh.is_prime(8273))
 
-        true_cases = [2, 3, 11, 1709, 5381, 8273]
-        false_cases = [1, 4, 3.14, [1, 2, 3]]
-
-        for true_case in true_cases:
-            self.assertTrue(mh.is_prime(true_case))
-        for false_case in false_cases:
-            self.assertFalse(mh.is_prime(false_case))
+        self.assertFalse(mh.is_prime(3.14))
+        self.assertFalse(mh.is_prime([1, 2, 3]))
