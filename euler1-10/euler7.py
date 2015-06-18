56327f0aaddcb5ba08b769e1efafbfeb64bb0edf
AlecTheDev <a.rasch@sportradar.com>
2015-06-18 10:29:10 +0200

    Added problem and strategy descriptions to problem 7.

--- euler1-10/euler7.py
+++ euler1-10/euler7.py
@@ -1,5 +1,15 @@
 __author__ = 'aleksander'
 
+'''
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+'''
+
+'''
+Strategy: Very straight-forward.
+'''
+
 import helpers.math_helper as mh
 
 print(mh.get_nth_prime(10001))
