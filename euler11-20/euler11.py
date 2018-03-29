5fbafda66d149f30f687237ed0a6c0d2fc17a568
alrasch <arasch855@gmail.com>
2018-03-29 02:10:51 +0200

    CLEANUP - Consolidate row/col loops, and cleanup for readability.

--- euler11-20/euler11.py
+++ euler11-20/euler11.py
@@ -14,35 +14,49 @@ class Euler11(AbstractSolution):
 
         running_highest = 0
 
-        #Rows
+        #Rows and columns
         for idx1 in range(0, 20):
             for idx2 in range(0, 17):
-                cells = [int(content[idx1][idx2]), int(content[idx1][idx2+1]), int(content[idx1][idx2+2]), int(content[idx1][idx2+3])]
-                product = mh.product_of_list(cells)
-                if product > running_highest:
-                    running_highest = product
+                row_cells = [int(content[idx1][idx2]),
+                             int(content[idx1][idx2+1]),
+                             int(content[idx1][idx2+2]),
+                             int(content[idx1][idx2+3])]
 
-        #Columns
-        for idx1 in range(0, 17):
-            for idx2 in range(0, 20):
-                cells = [int(content[idx1][idx2]), int(content[idx1+1][idx2]), int(content[idx1+2][idx2]), int(content[idx1+3][idx2])]
-                product = mh.product_of_list(cells)
-                if product > running_highest:
-                    running_highest = product
+                column_cells = [int(content[idx2][idx1]),
+                                int(content[idx2+1][idx1]),
+                                int(content[idx2+2][idx1]),
+                                int(content[idx2+3][idx1])]
+
+                row_product = mh.product_of_list(row_cells)
+                column_product = mh.product_of_list(column_cells)
+
+                if row_product > running_highest:
+                    running_highest = row_product
+                if column_product > running_highest:
+                    running_highest = column_product
 
         #Down/right diagonal
         for idx1 in range(0, 17):
             for idx2 in range(0, 17):
-                cells = [int(content[idx1][idx2]), int(content[idx1+1][idx2+1]), int(content[idx1+2][idx2+2]), int(content[idx1+3][idx2+3])]
+                cells = [int(content[idx1][idx2]),
+                         int(content[idx1+1][idx2+1]),
+                         int(content[idx1+2][idx2+2]),
+                         int(content[idx1+3][idx2+3])]
+
                 product = mh.product_of_list(cells)
-                if product > running_highest: running_highest = product
+                if product > running_highest:
+                    running_highest = product
 
         #Up/right diagonal
         for idx1 in range(0, 17):
             for idx2 in range(3, 20):
-                cells = [int(content[idx1][idx2]), int(content[idx1 + 1][idx2 - 1]), int(content[idx1 + 2][idx2 - 2]),
-                 int(content[idx1 + 3][idx2 - 3])]
+                cells = [int(content[idx1][idx2]),
+                         int(content[idx1+1][idx2-1]),
+                         int(content[idx1+2][idx2-2]),
+                         int(content[idx1+3][idx2-3])]
+
                 product = mh.product_of_list(cells)
-                if product > running_highest: running_highest = product
+                if product > running_highest:
+                    running_highest = product
 
         return running_highest
